93dc1572a3821d32f7efda81f0ced370cc6e2c7d
Meyappan <meysubb@gmail.com>
2019-01-13 08:35:44 -0600

    add sec teams

--- get_sec_teams_list.R
+++ get_sec_teams_list.R
@@ -4,8 +4,8 @@ library(ncaahoopR)
 url <- 'http://www.espn.com/mens-college-basketball/standings'
 sec_teams_raw <- read_html(url) %>% html_nodes("div:nth-child(26) .hide-mobile a") %>% html_text()
 sec_teams <- gsub("\\s*\\w*$", "", sec_teams_raw)
-sec_teams[9] <- "Alabama"
-sec_teams[13] <- "Miss St"
+sec_teams[grepl("Alabama",sec_teams)] <- "Alabama"
+sec_teams[grepl('Mississippi State',sec_teams)]<- "Miss St"
 teams <- ids
 match(sec_teams,teams$team)
 sec_teams[15] <- "Texas A&M TA&M" 
@@ -14,15 +14,15 @@ sec_teams[16] <- "Mississippi State"
 
 sec_df <- data.frame(sec_teams)
 
-sec_hashtags <- c("#HottyToddy #OleMiss","#Vols",
-                  "#HereSC #USc","#GeuxTigers #LSU",
-                  "#WarEagle","#BBN",
-                  "#ARKY #WPS","#Gators #UF",
-                  "#Bama","#UGA #DAWGS",
+sec_hashtags <- c("#Vols","#HottyToddy #OleMiss",
+                  "#GeuxTigers #LSU","#HereSC #USc",
+                  "#BBN","#WarEagle",
+                  "#ARKY #WPS","#Bama",
+                  "#Gators #UF","#UGA #DAWGS",
                   "#TAMU #Aggies","#Mizzou",
                   "#HailState #CLANGA","#AnchorDown #Vandy",
                  "#TAMU #Aggies","#HailState #CLANGA")
 
 sec_df$hashtags <- sec_hashtags
 
-saveRDS(sec_df,"sec_teams_list.RDS")
+saveRDS(sec_df,"app/sec_teams_list.RDS")
